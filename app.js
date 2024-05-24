c29c0c678be3deb15dc6d98c56d82dce3454b63d
Ali <eliqarayev835@gmail.com>
2024-05-24 22:13:10 +0400

    add header

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
 window.addEventListener("load", () => {
   new cursoreffects.trailingCursor();
 });
-document.addEventListener("click", type);
-
 let start = document.querySelector(".start");
-
+let header = document.querySelector(".header");
+document.addEventListener("click", type);
 function type() {
   start.classList.add("hide");
-
+  header.classList.remove("hide");
   const sampler = new Tone.Sampler({
     urls: {
       A0: "./keyboard.mp3",
+      A1: "./keyboard.mp3",
     },
     onload: () => {
-      sampler.triggerAttackRelease(["A0"], 12);
+      sampler.triggerAttackRelease(["A0"], 10);
     },
   }).toDestination();
 
   let typed = new Typed("#msg", {
     strings: [
-      `Hi there! I'm Eli, a frontend developer passionate about crafting user-friendly interfaces and building beautiful web apps. I love making music with Tone.js. Check out my projects on <a href="https://github.com/rhy6x">GitHub</a>. Connect with me on <a href="https://www.linkedin.com/in/eli-qarayev/">LinkedIn</a> for more!`,
+      `Hi, I'm Eli, a passionate Front-End Developer skilled in <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>TypeScript</b>, <b>React</b>, and <b>Next.js</b>. I create responsive, user-friendly web applications and constantly seek to improve my skills and learn new technologies. Connect with me on <a href="https://www.linkedin.com/in/eli-qarayev/">LinkedIn</a> `,
     ],
-    typeSpeed: 32,
+
+    typeSpeed: 25,
     showCursor: false,
   });
 
