448416e1dc6658331d558d6d90108ecb810a5a0c
Ali <eliqarayev835@gmail.com>
2024-05-24 23:45:52 +0400

    test

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 window.addEventListener("load", () => {
   new cursoreffects.trailingCursor();
+  let start = document.querySelector(".start");
+  let header = document.querySelector(".header");
+  document.addEventListener("click", type);
+  function type() {
+    start.classList.add("hide");
+    header.classList.remove("hide");
+    const sampler = new Tone.Sampler({
+      urls: {
+        A0: "./keyboard.mp3",
+        A1: "./keyboard.mp3",
+      },
+      onload: () => {
+        sampler.triggerAttackRelease(["A0"], 10);
+      },
+    }).toDestination();
+
+    let typed = new Typed("#msg", {
+      strings: [
+        `Hi, I'm Eli, a passionate Front-End Developer skilled in <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>TypeScript</b>, <b>React</b>, and <b>Next.js</b>. I create responsive, user-friendly web applications and constantly seek to improve my skills and learn new technologies. Connect with me on <a href="https://www.linkedin.com/in/eli-qarayev/">LinkedIn</a> `,
+      ],
+
+      typeSpeed: 25,
+      showCursor: false,
+    });
+
+    document.removeEventListener("click", type);
+  }
 });
-let start = document.querySelector(".start");
-let header = document.querySelector(".header");
-document.addEventListener("click", type);
-function type() {
-  start.classList.add("hide");
-  header.classList.remove("hide");
-  const sampler = new Tone.Sampler({
-    urls: {
-      A0: "./keyboard.mp3",
-      A1: "./keyboard.mp3",
-    },
-    onload: () => {
-      sampler.triggerAttackRelease(["A0"], 10);
-    },
-  }).toDestination();
-
-  let typed = new Typed("#msg", {
-    strings: [
-      `Hi, I'm Eli, a passionate Front-End Developer skilled in <b>HTML</b>, <b>CSS</b>, <b>JavaScript</b>, <b>TypeScript</b>, <b>React</b>, and <b>Next.js</b>. I create responsive, user-friendly web applications and constantly seek to improve my skills and learn new technologies. Connect with me on <a href="https://www.linkedin.com/in/eli-qarayev/">LinkedIn</a> `,
-    ],
-
-    typeSpeed: 25,
-    showCursor: false,
-  });
-
-  document.removeEventListener("click", type);
-}
