5a7fc811217bf40184e2dcb08d7aa88d1b5d070a
Ali <eliqarayev835@gmail.com>
2024-05-24 21:35:32 +0400

    speed changes

--- app.js
+++ app.js
@@ -7,6 +7,16 @@ let start = document.querySelector(".start");
 
 function type() {
   start.classList.add("hide");
+
+  const sampler = new Tone.Sampler({
+    urls: {
+      A0: "./keyboard.mp3",
+    },
+    onload: () => {
+      sampler.triggerAttackRelease(["A0"], 12);
+    },
+  }).toDestination();
+
   let typed = new Typed("#msg", {
     strings: [
       `Hi there! I'm Eli, a frontend developer passionate about crafting user-friendly interfaces and building beautiful web apps. I love making music with Tone.js. Check out my projects on <a href="https://github.com/rhy6x">GitHub</a>. Connect with me on <a href="https://www.linkedin.com/in/eli-qarayev/">LinkedIn</a> for more!`,
@@ -14,15 +24,6 @@ function type() {
     typeSpeed: 32,
     showCursor: false,
   });
-
-  const sampler = new Tone.Sampler({
-    urls: {
-      A0: "./keyboard.mp3",
-    },
-    onload: () => {
-      sampler.triggerAttackRelease(["A0"], 15);
-    },
-  }).toDestination();
 
   document.removeEventListener("click", type);
 }
