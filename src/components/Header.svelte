77b1da2d5ea79dbd45593d213de2f0fd6ad87a2a
Ali <eliqarayev835@gmail.com>
2024-08-26 00:22:51 +0400

    add overlay

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,45 +1,82 @@
-<script>
+<script lang="ts">
   let logo = "/icons/logo.png";
+  let isMenuOpen = false;
+  function handleInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+    isMenuOpen = target.checked;
+  }
 </script>
 
 <header>
-  <div class="logo">
-    <a href="/">
-      <img src={logo} alt="" style="width: 4.375rem; height:4.375rem" />
-    </a>
+  {#if isMenuOpen}
+    <div class="overlay">
+      <div class="menu">
+        <div class="menu-link">
+          <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">PROJECTS</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">CONTACT</a></li>
+          </ul>
+        </div>
+        <div class="menu-contacts">
+          <span>testl@test.com</span>
+          <span>050 412 40 32</span>
+        </div>
+      </div>
+    </div>
+  {/if}
+  <div class="header">
+    <div class="logo">
+      <a href="/">
+        <img src={logo} alt="" style="width: 4.375rem; height:4.375rem" />
+      </a>
+    </div>
+
+    <div class="menu-button">
+      <input
+        type="checkbox"
+        id="toggle"
+        checked={isMenuOpen}
+        on:change={handleInput}
+      />
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </div>
   </div>
-  <div class="menu">
-    <input type="checkbox" id="toggle" />
-    <span></span>
-    <span></span>
-    <span></span>
-  </div>
+  {#if !isMenuOpen}
+    <div class="header-text">
+      <div class="text">
+        <h1>ELI GARAEV</h1>
+        <span>FRONTEND WEB DEVELOPER </span>
+        <br />
+        <br />
+        <button class="btn"> My Resume </button>
+      </div>
+    </div>
+  {/if}
 </header>
 
-<div class="text">
-  <h1>ELI GARAEV</h1>
-  <span>FRONTEND WEB DEVELOPER </span>
-  <br />
-  <br />
-  <button class="btn"> My Resume </button>
-</div>
-
 <style>
-  header {
+  .header {
     display: flex;
-    align-items: center;
+    width: 100%;
     justify-content: space-between;
-    animation: animation-2 2s ease-in-out forwards;
+    align-items: center;
+    padding-top: 30px;
+    animation: animation-2 2s ease-out forwards;
     position: sticky;
     top: 0;
   }
-  .menu {
+  .menu-button {
     width: 3.75rem;
     height: 1.875rem;
     position: relative;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    z-index: 999;
   }
   .btn {
     padding: 10px 20px;
@@ -63,7 +100,6 @@
     right: -3px;
     width: 255px;
     height: 65px;
-    /* filter: blur(1px); */
     z-index: -1;
     background: linear-gradient(40deg, #790e8f, #05b86d, #977d07, #d00a0a);
     background-size: 200%;
@@ -97,14 +133,19 @@
     height: 100%;
     width: 100%;
     opacity: 0;
-    z-index: 9;
+    z-index: 999;
   }
-  .menu span {
+
+  .line {
     display: block;
     height: 0.1875rem;
     width: 3.125rem;
     background: white;
     transition: 0.3s transform;
+    z-index: 999;
+  }
+  #toggle:checked ~ .line {
+    background: black;
   }
   #toggle:checked ~ span:nth-of-type(2) {
     transform: scale(0);
@@ -170,5 +211,45 @@
       transform: translateX(0%);
       opacity: 1;
     }
+  }
+
+  /* Overlay  */
+  .overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+  }
+
+  .menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 200px;
+    height: 100vh;
+    font-size: 40px;
+    color: black;
+    animation: animation-2 1.5s ease forwards;
+  }
+  .menu li {
+    list-style: none;
+    transition: 0.3s ease transform;
+  }
+  .menu li a {
+    color: black;
+    font-size: 64px;
+    text-decoration: none;
+    position: relative;
+  }
+  .menu li:hover {
+    transform: scaleX(50%);
+  }
+  .menu-contacts {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    font-size: 24px;
   }
 </style>
