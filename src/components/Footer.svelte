03d797db81de48a68c7cc099b7ef411a08581c5d
Ali <eliqarayev835@gmail.com>
2024-08-28 00:21:33 +0400

    add footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,1 +1,49 @@
-<h1>FOOTER</h1>
+<script>
+  let logo = "/icons/logo.png";
+</script>
+
+<footer>
+  <ul>
+    <li><a href="#">PROJECTS</a></li>
+    <li><a href="#">ABOUT</a></li>
+    <li><a href="#">CONTACT</a></li>
+  </ul>
+  <div class="contact">
+    <span>050 412 40 32</span>
+    <a href="mailto:example@example.com">example@example.com</a>
+  </div>
+  <div class="logo">
+    <a href="#">
+      <img src={logo} alt="logo" />
+    </a>
+  </div>
+</footer>
+
+<style>
+  footer {
+    padding: 100px 0px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  ul {
+    display: flex;
+    gap: 20px;
+  }
+
+  .contact {
+    display: flex;
+    flex-direction: column;
+  }
+  .contact a,
+  ul a {
+    font-size: 25px;
+    color: white;
+  }
+  .contact span {
+    font-size: 25px;
+  }
+  img {
+    width: 200px;
+  }
+</style>
