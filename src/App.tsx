a8ca5e7e08c3790a65f76232e2d35cf7ca79a230
e-qa <eliqarayev835@gmail.com>
2024-09-18 00:35:15 +0400

    add react-router and configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="align-element">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
