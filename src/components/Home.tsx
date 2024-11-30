7822918e2233621ed82231a18dcddd8c76109e53
e-qa <eliqarayev835@gmail.com>
2024-11-30 23:52:18 +0400

    add responsive class

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,7 +18,7 @@ const Home = () => {
 
   return (
     <motion.main
-      className="pt-20 px-20 h-full flex flex-col gap-10"
+      className="pt-20 lg:px-20 px-0 h-full flex flex-col gap-10"
       variants={container}
       initial="hidden"
       animate="visible"
